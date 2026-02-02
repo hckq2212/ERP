1f69f0a8ff96b2d32efed8ceebec2a3d7465548f
quangkid2212 <163752892+hckq2212@users.noreply.github.com>
2026-02-02 15:06:14 +0700

    config cors

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://erp-gegn8by1b-quangs-projects-4fcd4940.vercel.app"],
     credentials: true
 }))
 app.use(passport.initialize())
